b8a1e657fed66c8cf339bc3fbe340f824a07c391
Vinicius Geisler <vinicius.geisler138@gmail.com>
2022-03-22 07:33:27 -0300

    Adiçao do Docker e configuraçao

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
-app.listen(4000, () => console.log("Server is running! 🚀"));
+app.listen(4000, () => console.log("Server is running!"));
