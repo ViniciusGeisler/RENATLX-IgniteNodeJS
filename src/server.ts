33f2981fd24bcb9e07c5c7c99c0e382ee0e767a3
Vinicius Geisler <vinicius.geisler138@gmail.com>
2022-03-25 08:19:21 -0300

    Criacao do container do banco, configuracao do postgres e conexao ao banco

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 import { router } from "./routes";
 
+import "./database";
+
 const app = express();
 
 app.use(express.json());
@@ -11,4 +13,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
-app.listen(4000, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running!"));
