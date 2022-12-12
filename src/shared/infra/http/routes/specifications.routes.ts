d63da6c4b98dc1dbbb258bf0ed7ae2f995a17fd6
Vinicius Geisler <vinicius.geisler138@gmail.com>
2022-12-12 07:02:22 -0300

    created admin middlware to routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
-import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 
 const specificationsRoutes = Router();
 
 const createSpecificationCOntroller = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationCOntroller.handle);
+specificationsRoutes.post(
+    "/",
+    ensureAuthenticated, 
+    ensureAdmin,
+    createSpecificationCOntroller.handle
+);
 
 export { specificationsRoutes };
