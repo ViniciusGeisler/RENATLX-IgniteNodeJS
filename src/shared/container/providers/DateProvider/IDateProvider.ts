ca4644b9d9a90ad4c156c9ccddf3c081ebaf56ea
Vinicius Geisler <vinicius.geisler138@gmail.com>
2023-01-10 07:48:17 -0300

    create refresh_token structure

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -6,6 +6,7 @@ interface IDateProvider {
   convertToUTC(date: Date): string;
   dateNow(): Date;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider }
