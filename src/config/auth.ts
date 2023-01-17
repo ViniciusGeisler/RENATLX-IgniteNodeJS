d3342856fc4a39ec4bb4de65bf34df32a0e6e349
Vinicius Geisler <viniciusgeisler@MacBook-Pro-de-Vinicius.local>
2023-01-17 16:19:24 -0300

    fix refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default { 
   secret_token: "57913d1c7839c2d4b6e26e9b99ba34e3",
-  expires_in_token: "15m",
+  expires_in_token: "30s",
   secret_refresh_token: "7ffd6039d441df05ac9736bfced74e12",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { secret_refresh_token, expires_refresh_token_days, expires_in_refresh_token } = auth;
 
     const { email, sub } = verify(token, secret_refresh_token) as IPayload;
@@ -45,9 +50,17 @@ class RefreshTokenUseCase {
       expires_date,
       refresh_token,
       user_id,
-    })
+    });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return {
+      refresh_token,
+      token: newToken
+    };
 
   }
 
